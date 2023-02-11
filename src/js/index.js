af84637c850ddd0b8369432de828fdbc76b8180f
Kieran Croft <kiecroft@gmail.com>
2023-02-11 00:42:31 +0000

    sticker ui added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 import '../css/style.css'
-console.log("hello world");
+
+document.querySelector('.mode').addEventListener('click', (e)=>{
+    if(e.target.textContent === 'Sticker') {
+        e.target.textContent = "GIF";
+        document.querySelectorAll('.button-search').forEach((button)=>{
+            button.style.backgroundColor = "var(--gif-color)";
+        });
+        document.querySelector('.mode').style.border = "5px solid var(--gif-color)";
+    } else {
+        e.target.textContent = "Sticker";
+        document.querySelectorAll('.button-search').forEach((button)=>{
+            button.style.backgroundColor = "var(--sticker-color)";
+        });
+        document.querySelector('.mode').style.border = "5px solid var(--sticker-color)";
+    }
+});
